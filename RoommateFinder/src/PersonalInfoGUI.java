e488c091d0e6def89f1f6d475e1f5bc5c758a3ed
William <williamv1217@gmail.com>
2016-05-02 23:10:32 -0700

    GUI for personal info and room preferences

---
+++ RoommateFinder/src/PersonalInfoGUI.java
@@ -0,0 +1,111 @@
+import javax.swing.*;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by William Vagharfard on 4/29/16.
+ */
+public class PersonalInfoGUI {
+
+    private JTextField majorText;
+    private JComboBox yearBox;
+    private JPanel PersonalInfoPanel;
+    private JRadioButton earlySleepButton;
+    private JRadioButton lateSleepButton;
+    private JRadioButton earlyWakeButton;
+    private JRadioButton lateWakeButton;
+    private JRadioButton quietButton;
+    private JRadioButton livelyButton;
+    private JButton previousButton;
+    private JButton nextButton;
+    private List<String> personalInfoList;
+    private final Charset UTF8 = StandardCharsets.UTF_8;
+
+
+    static void createFrame() {
+        JFrame frame = new JFrame();
+        frame.setContentPane(new PersonalInfoGUI().PersonalInfoPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public PersonalInfoGUI() {
+        personalInfoList = new ArrayList<>();
+
+        nextButton.addActionListener(e -> {
+            personalInfoList.add(getMajor());
+            personalInfoList.add(getYear());
+            personalInfoList.add(getSleepPrefs());
+            personalInfoList.add(getWakePrefs());
+            personalInfoList.add(getRoommatePref());
+
+            try {
+                Files.write(Paths.get("PersonalInfo.txt"), personalInfoList, UTF8,
+                        StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            } catch (IOException exc) {
+                exc.printStackTrace();
+            }
+
+            ((JFrame) PersonalInfoPanel.getTopLevelAncestor()).dispose();
+            RoomPreferencesGUI.createFrame();
+        });
+
+        previousButton.addActionListener(e -> {
+            //TODO: Add actionListener to Go to Ray's screen
+            // ((JFrame) PersonalInfoPanel.getTopLevelAncestor()).dispose();
+            // ________.createFrame();
+        });
+    }
+
+
+    private String getMajor(){
+        if (majorText.getText().length() == 0)
+            return "Undeclared";
+        else
+            return majorText.getText();
+    }
+
+    private String getYear() {
+        return (String) yearBox.getSelectedItem();
+    }
+
+    private String getSleepPrefs() {
+        if (earlySleepButton.isSelected())
+            return "Early";
+        else if (lateSleepButton.isSelected())
+            return "Late";
+        else
+            return "No preference";
+    }
+
+    private String getWakePrefs() {
+        if (earlyWakeButton.isSelected())
+            return "Early";
+        else if (lateWakeButton.isSelected())
+            return "Late";
+        else
+            return "No preference";
+    }
+
+    private String getRoommatePref() {
+        if (quietButton.isSelected())
+            return "Quiet";
+        else if (livelyButton.isSelected())
+            return "Lively";
+        else
+            return "No preference";
+    }
+
+
+    public static void main(String[] args){
+        PersonalInfoGUI.createFrame();
+    }
+}
---
+++ RoommateFinder/src/RoomPreferencesGUI.java
@@ -0,0 +1,145 @@
+import com.sun.xml.internal.bind.api.impl.NameConverter;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by William Vagharfard on 4/29/16.
+ */
+public class RoomPreferencesGUI {
+    private JRadioButton lowPriceButton;
+    private JRadioButton kitchenYesButton;
+    private JRadioButton showerYesButton;
+    private JRadioButton floorLowerButton;
+    private JRadioButton bunkLowerButton;
+    private JRadioButton roomLightButton;
+    private JRadioButton wdYesButton;
+    private JRadioButton bunkUpperButton;
+    private JRadioButton highPriceButton;
+    private JRadioButton roomDarkButton;
+    private JRadioButton kitchenNoButton;
+    private JRadioButton floorUpperButton;
+    private JRadioButton wdNoButton;
+    private JRadioButton showerNoButton;
+    private JButton previousButton;
+    private JButton nextButton;
+    private JPanel RoomPreferencesPanel;
+    private List<String> roomPreferencesList;
+    private final Charset UTF8 = StandardCharsets.UTF_8;
+
+    static void createFrame(){
+        JFrame frame = new JFrame();
+        frame.setContentPane(new RoomPreferencesGUI().RoomPreferencesPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public RoomPreferencesGUI(){
+        roomPreferencesList = new ArrayList<>();
+
+        nextButton.addActionListener(e -> {
+            roomPreferencesList.add(pricePref());
+            roomPreferencesList.add(kitchenPref());
+            roomPreferencesList.add(showerPref());
+            roomPreferencesList.add(washerDryerPref());
+            roomPreferencesList.add(floorPref());
+            roomPreferencesList.add(roomBrightnessPref());
+            roomPreferencesList.add(bunkPref());
+
+            try {
+                Files.write(Paths.get("RoomPreferences.txt"), roomPreferencesList, UTF8,
+                        StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            } catch (IOException exc) {
+                exc.printStackTrace();
+            }
+
+            //TODO: link with Next screen
+            // ((JFrame) RoomPreferencesPanel.getTopLevelAncestor()).dispose();
+            // _________.createFrame();
+        });
+
+        previousButton.addActionListener(e -> {
+            //TODO: anything else to do here besides moving to previous screen?
+
+            ((JFrame) RoomPreferencesPanel.getTopLevelAncestor()).dispose();
+            PersonalInfoGUI.createFrame();
+        });
+    }
+
+    private String pricePref() {
+        if (lowPriceButton.isSelected())
+            return "Low";
+        else if (highPriceButton.isSelected())
+            return "High";
+        else
+            return "No preference";
+    }
+
+    private String kitchenPref() {
+        if (kitchenNoButton.isSelected())
+            return "No";
+        else if (kitchenYesButton.isSelected())
+            return "Yes";
+        else
+            return "No preference";
+    }
+
+    private String showerPref() {
+        if (showerNoButton.isSelected())
+            return "No";
+        else if (showerYesButton.isSelected())
+            return "Yes";
+        else
+            return "No preference";
+    }
+
+    private String washerDryerPref() {
+        if (wdNoButton.isSelected())
+            return "No";
+        else if (wdYesButton.isSelected())
+            return "Yes";
+        else
+            return "No preference";
+    }
+
+    private String floorPref() {
+        if (floorLowerButton.isSelected())
+            return "Lower";
+        else if (floorUpperButton.isSelected())
+            return "Upper";
+        else
+            return "No preference";
+    }
+
+    private String roomBrightnessPref() {
+        if (roomDarkButton.isSelected())
+            return "Dark";
+        else if (roomLightButton.isSelected())
+            return "Light";
+        else
+            return "No preference";
+    }
+
+    private String bunkPref() {
+        if (bunkLowerButton.isSelected())
+            return "Lower";
+        else if (bunkUpperButton.isSelected())
+            return "Upper";
+        else
+            return "No preference";
+    }
+
+    public static void main(String[] args){
+        RoomPreferencesGUI.createFrame();
+    }
+
+}
