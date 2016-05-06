73c69dd88ef2928aebccbc5d101820cec862a17d
GevorkianRay <gevorkianray@gmail.com>
2016-05-05 20:35:26 -0700

    Update Tester.java

--- RoommateFinder/src/Tester.java
+++ RoommateFinder/src/Tester.java
@@ -1,3 +1,5 @@
+import java.util.Queue;
+
 /**
  * This is a tester in the stead of the GUI, which we are currently waiting on.
  * @author Matt
@@ -8,13 +10,32 @@ public class Tester
 	public static void main(String args[])
 	{
 		School sjsu = new School("SJSU");
-		School.Building Cvb = new School.Building("CVB");
+		School.Building cvb = new School.Building("CVB");
 		School.Building.Room room101 = new School.Building.Room(4);
-		Student you = new Student("Bob", new int[]{20, 1, 1, 1, 10, 8});
-		while(you.getKey() == 0) you.setKey(Password.createPassword("pass", "pass"));
-		sjsu.add(Cvb); Cvb.add(room101);
+		School.Building.Room room102 = new School.Building.Room(4);
 		
-		System.out.println(sjsu.add(you, Cvb, room101) + "" + you.getKey());
-		//There's probably a better way to implement this student, room, building, school paradigm
+		sjsu.add(cvb); cvb.add(room101); cvb.add(room102);
+		
+		Student you = new Student("Bob", new int[]{1, 1, 1, 1, 1, 1});
+		Student wu = new Student("Wu Zetan", new int[]{5, 5, 5, 5, 5, 5});
+		Student nap = new Student("Napolean Bonaparte", new int[]{1, 2, 3, 4, 5, 6});
+		Student cptc = new Student("Captain Crunch", new int[]{6, 3, 1, 6, 3, 2});
+		
+		while(you.getKey() == 0) you.setKey(Password.createPassword("pass", "pass"));
+		
+		sjsu.add(you, cvb, room101);
+		sjsu.add(wu, cvb, room101);
+		sjsu.add(cptc, cvb, room101);
+		
+		sjsu.add(Student.generate(), cvb, room101);
+		for(int i = 0; i < 6; i++){sjsu.add(Student.generate(), cvb, room102);}
+		
+		Queue<Student> matches = you.matches();
+		
+		System.out.println("Name\t\t\t" + "Number of similarities to You\t");
+		for(Student match : matches)
+		{
+			System.out.println(match.getName() + "\t\t\t" + match.compareTo(you));
+		}
 	}
 }
